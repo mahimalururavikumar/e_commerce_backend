fffeeaba4453a4a42824779e27574e2250cc8717
Ravikumar <mvravikuamr6301@gmail.com>
2024-10-29 20:56:46 +0530

    token modified

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (user) => {
-    return jwt.sign({email : user.email,id: user._id}, process.env.JWT_KEY || 'ravi',{ expiresIn: '1h' })
-}
+    const secretKey = process.env.JWT_KEY;
+    if (!secretKey) {
+        throw new Error('JWT Secret Key is not defined. Set the JWT_KEY environment variable.');
+    }
+
+    return jwt.sign(
+        { email: user.email, id: user._id },
+        secretKey,
+        { expiresIn: '1h', algorithm: 'HS512' } // explicitly using HS512 for added security
+    );
+};
 
 module.exports.generateToken = generateToken;
